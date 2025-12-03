de8159ac044d8d2278ed2ffdfa1cdacb9333a805
Arturo Roberti <105200773+ArturoRoberti@users.noreply.github.com>
2025-12-03 12:41:15 +0100

    Edit clip function
    Changed clip alias to clip function with suppress flag

--- templates/aliases.bash
+++ templates/aliases.bash
@@ -1,5 +1,14 @@
 # Git
 export EDITOR="code --wait"
 
-# Easy copy to clipboard - Usage: "command_with_output | clip"
-alias clip='xclip -selection clipboard'
+# Easy copy to clipboard - Usage: "command_with_output | clip [-s|--suppress]"
+clip() {
+    local xc='xclip -selection clipboard'
+    if [[ " $* " == *" -s "* || " $* " == *" --suppress "* ]]; then
+        # Suppressed: only send to clipboard
+        $xc
+    else
+        # Normal: show + send to clipboard
+        tee >($xc)
+    fi
+}
