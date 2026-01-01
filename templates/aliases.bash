a14034f7d5d7fdcb589685077ab23056d098138b
ArturoRoberti <arturo.roberti@outlook.com>
2026-01-01 18:30:35 +0100

    Updated bash indentation and added GIT_EDITOR

--- templates/aliases.bash
+++ templates/aliases.bash
@@ -1,14 +1,16 @@
 # Git
-export EDITOR="code --wait"
+if command -v code >/dev/null 2>&1; then
+	export GIT_EDITOR="code --wait"
+fi
 
 # Easy copy to clipboard - Usage: "command_with_output | clip [-s|--suppress]"
 clip() {
-    local xc='xclip -selection clipboard'
-    if [[ " $* " == *" -s "* || " $* " == *" --suppress "* ]]; then
-        # Suppressed: only send to clipboard
-        $xc
-    else
-        # Normal: show + send to clipboard
-        tee >($xc)
-    fi
+	local xc='xclip -selection clipboard'
+	if [[ " $* " == *" -s "* || " $* " == *" --suppress "* ]]; then
+		# Suppressed: only send to clipboard
+		$xc
+	else
+		# Normal: show + send to clipboard
+		tee >($xc)
+	fi
 }
